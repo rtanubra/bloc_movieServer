9535e5c34ba257587e37f545d42026977e77a3ec
rtanubra <rdtanubrata@gmail.com>
2019-05-22 18:52:30 -0400

    implementing authorization

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const MOVIELIST= require('./movies-data.json')
 
 const app = express()
 app.use(morgan('dev'))
+app.use(AuthorizeToken)
 
 require('dotenv').config() //need to install dotenv
 const API_TOKEN = process.env.API_TOKEN
+
+function AuthorizeToken(req,res,next){
+    const authorization = req.get('Authorization')
+    
+    console.log(authorization)
+    if (!authorization){
+        return res.status(401).json({error:"Unauthorized, please provide an api token"})
+    }
+    if (authorization.split(" ")[1] !== API_TOKEN){
+        return res.status(401).json({error:"Unauthorized, please provide a VALID api token"})
+    }
+    next()
+}
 
 function capitalizeCorrectly(str){
     const myArr = str.split(" ")//split by space
@@ -21,7 +35,7 @@ function getMovies(req,res){
     const { country = "" } = req.query
     const { avg_vote = "" } = req.query 
     if (!genre && !country && !avg_vote ){
-        res.status(400).json({error:"Required Params: at least one of three [genre,country,avg_vote]"})
+        return res.status(400).json({error:"Required Params: at least one of three [genre,country,avg_vote]"})
     }
 
     const genreCorrected = genre? capitalizeCorrectly(genre) : ""
@@ -39,15 +53,15 @@ function getMovies(req,res){
     //filter by avg_vote
     if (avg_vote){
         if (isNaN(avg_vote)){
-            res.status(400).json({error:"avg_vote should be a number between 0.1 to 9.9"})
+            return res.status(400).json({error:"avg_vote should be a number between 0.1 to 9.9"})
         }
         if (Number(avg_vote) <0 || Number(avg_vote) > 9.9 ){
-            res.status(400).json({error:"avg_vote should be a number between 0.1 to 9.9"})
+            return res.status(400).json({error:"avg_vote should be a number between 0.1 to 9.9"})
         }
         const moviesByRating = moviesByCountry.filter((movie)=>{
             return Number(movie.avg_vote) >= Number(avg_vote)
         })
-        res.json(moviesByRating)
+        return res.json(moviesByRating)
     }
     
 
